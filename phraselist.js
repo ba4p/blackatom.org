0399140516628aefda7b598d7b19d313849cad41
z3r0bu6 <72890222+z3r0bu6@users.noreply.github.com>
2022-07-15 20:56:52 +0200

    Update phraselist.js

--- phraselist.js
+++ phraselist.js
@@ -2,6 +2,5 @@ var phraselist = [
 "Investigation ongoing",
 "Calculating Threat",
 "Searching for Private Data",
-"Searching for Security Data",
-"Downloading Private and Security Data",
+"Downloading Private Data",
 ]
